9e15f14acca99ead54cb640500731ff04ef5724c
ast0m <gavomaza1@gmail.com>
2024-07-31 02:29:32 -0600

    Ejemplos break & continue

--- Modulo 3 Valores booleanos, bucles, listas, etc./01File.py
+++ Modulo 3 Valores booleanos, bucles, listas, etc./01File.py
@@ -246,4 +246,67 @@
 
 #"Prueba para git"
 
+#for varA in range(1,11):
+#    for varB in range(1,11):
+#        print(varA, "X", varB, "=", varA * varB )
+#        if varB == 10:
+#            print("\n")
+#print("fuera del for")
 
+"Las Sentencias break y continue"
+#
+## break - ejemplo
+#
+#print("la instrucción break:")
+#for i in range(1, 6):
+#    if i == 3:
+#        break
+#    print("Dentro del bucle.", i)
+#print("Fuera del bucle")
+
+
+# continue - ejemplo
+
+#print("\nLa instrucción continue: ")
+#for i in range(1, 6):
+#    if i == 3:
+#        continue
+#    print("Dentro del bucle. ", i)
+#print("Fuera del bucle.")
+
+# Las sentencias break y continue: más  ejemplos
+
+#largest_number = -99999999
+#counter = 0
+#
+#while True:
+#    number = int(input("Ingresa un número o escribe -1 para finalizar el programa:"))
+#    if number == -1:
+#        break
+#    counter += 1
+#    if number > largest_number:
+#        largest_number = number
+#        
+#if counter != 0:
+#    print("El número más grande es: ", largest_number)
+#else:
+#    print("No has ingresado ningún número.")
+#    
+    
+largest_number = -9999999
+counter = 0
+
+number = int(input("Ingresa un número o escribe -1 para finalizar el programa: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+    
+    if number > largest_number:
+        largest_number = number
+    number = int(input("Ingresa un número o escribe -1 para finalizar el programa: "))
+if counter:
+    print("El número más grande es", largest_number)
+else:
+    print("No has ingresado ningún número.") 
