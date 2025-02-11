0080b009157865a928610e065133dd78677853f3
ast0m <gavomaza1@gmail.com>
2025-02-11 16:21:31 -0600

    Operadores logicos y bit a bit

--- Modulo 3 Valores booleanos, bucles, listas, etc./Seccion 3 - Operadores logicos y operaciones bit a bit/seccion3.py
+++ Modulo 3 Valores booleanos, bucles, listas, etc./Seccion 3 - Operadores logicos y operaciones bit a bit/seccion3.py
@@ -1,11 +1,11 @@
 """Expresiones logicas"""
 
-var = 1
+# var = 1
 
-#Ejemplo 1
-print( var > 0 )
-print( not (var <= 0))
+# #Ejemplo 1
+# print( var > 0 )
+# print( not (var <= 0))
 
-#Ejemplo 2
-print(var != 0)
-print(not (var == 0))
+# #Ejemplo 2
+# print(var != 0)
+# prin(not (var == 0))
