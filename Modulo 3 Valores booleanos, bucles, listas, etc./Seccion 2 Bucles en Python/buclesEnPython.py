6606ac62cfdfeeb3450e101bda3e7d813f73f980
ast0m <gavomaza1@gmail.com>
2025-02-11 16:15:55 -0600

    while, for, break & continue

--- Modulo 3 Valores booleanos, bucles, listas, etc./Seccion 2 Bucles en Python/buclesEnPython.py
+++ Modulo 3 Valores booleanos, bucles, listas, etc./Seccion 2 Bucles en Python/buclesEnPython.py
@@ -139,28 +139,28 @@
 
 # Example of Break
 
-print("Sentencia break")
-for i in range(1,6):
-    print("Ciclo for", i)
-    if i == 3:
-        print("Se ejecutará una sentencia 'break'")
-        break
-print("Fuera del ciclo")
+# print("Sentencia break")
+# for i in range(1,6):
+#     print("Ciclo for", i)
+#     if i == 3:
+#         print("Se ejecutará una sentencia 'break'")
+#         break
+# print("Fuera del ciclo")
 
 
-print("Sentencia continue")
-for i in range(1,6):
-    print("Valor de i es: ", i )
-    if i == 2:
-        print("Se ejecutará una sentencia 'continue'")       
-        continue
-    if i == 4:
-        print("Se ejecutará una sentencia 'continue'")       
-        continue
-print("Fuera del ciclo")
+# print("Sentencia continue")
+# for i in range(1,6):
+#     print("Valor de i es: ", i )
+#     if i == 2:
+#         print("Se ejecutará una sentencia 'continue'")       
+#         continue
+#     if i == 4:
+#         print("Se ejecutará una sentencia 'continue'")       
+#         continue
+# print("Fuera del ciclo")
     
 
-
+    
 
 
 #----------------------------------------------------------------------
@@ -191,12 +191,110 @@ print("Fuera del ciclo")
 # print("Hecho, muggle! Eres libre ahora")
 
 
-'''
-****************************************
-* Estos son ejercicios con for         *
-*                                      *
-*                                      *
-****************************************
+# """
+# ****************************************
+# * Estos son ejercicios con for         *
+# *                                      *
+# *                                      *
+# ****************************************
 
-'''
+# """
 
+"""Sentencia break - atrapado en un blucle"""
+
+
+# input_word = str(input("Escribe una palabra para adivinar la palabra oculta: "))
+# word_hidden  = "chupacabras"
+# while word_hidden:
+#     if input_word == word_hidden:
+#         break
+#     else:
+#         print("La palabra ", input_word , " No es la palabra oculta")
+#         input_word = str(input("Escribe una palabra para adivinar la palabra oculta"))
+        
+# print("has dejado el ciclo")
+
+
+"""Sentencia continue - el Feo Devorador de Vocales"""
+# Programa que usa un bucle for con un condicional (if-elif-else) y la sentencia continue
+# El programa pide al usuario que ingrese la palabra utiliza el metodo .upper() para convertir la palabra en mayusculas 
+# utilizar la instruccion continue para devorar las vocales, despues imprimir las palabras sin las vocales devoradas
+
+# user_word = str(input("Ingresa una palabra: "))
+# user_word = user_word.upper()
+# user_word_final = ""
+# for vowel in user_word:
+#     if vowel == "A":
+#         continue    
+#     elif vowel == "E":
+#         continue
+#     elif vowel == "I":
+#         continue
+#     elif vowel == "O":
+#         continue
+#     elif vowel == "U":
+#         continue
+#     user_word_final += vowel
+#     print("letra dentro del ciclo for: ", vowel)
+# print(user_word_final)
+
+
+
+# Bloque while y el bloque else
+
+# i = 1
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print("Bloque else:" , i)
+
+
+"""Bloque for y bloque else"""
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print("else: ", i)
+
+
+"""Pregunta 1: Crea un bucle for que cuente de 0 a 10, e imprima números impares en la pantalla. Usa el esqueleto de abajo:"""
+
+
+# for i in range(1, 11):
+#     if i % 2 != 0:
+#         print(i)
+#         # Línea de código.
+
+"""
+    
+Pregunta 3: Crea un programa con un bucle for y una sentencia break. El programa debe iterar sobre los caracteres en una dirección de correo electrónico, salir del bucle cuando llegue al símbolo @ e imprimir la parte antes de @ en una línea. Usa el esqueleto de abajo:
+
+"""
+    
+   
+# for ch in "john.smith@pythoninstitute.org":
+#     if ch == "@":
+#         break
+#     print(ch, end="")
+
+
+"""Pregunta 2: Crea un bucle while que cuente de 0 a 10, e imprima números impares en la pantalla. Usa el esqueleto de abajo:"""
+
+
+# x = 1
+# while x < 11:
+#     if x % 2 != 0:
+#         print(x)
+#     x += 1
+
+
+"""Pregunta 4: Crea un programa con un bucle for y una sentencia continue. El programa debe iterar sobre una cadena de dígitos, reemplazar cada 0 con x, e imprimir la cadena modificada en la pantalla. Usa el esqueleto de abajo:"""
+
+# for digit in "0165031806510":
+#     if digit == "0":
+#         print("X", end="")
+#         continue
+#     print(digit, end="")
+
+ 
---
+++ Modulo 3 Valores booleanos, bucles, listas, etc./Seccion 3 - Operadores logicos y operaciones bit a bit/seccion3.py
@@ -0,0 +1,11 @@
+"""Expresiones logicas"""
+
+var = 1
+
+#Ejemplo 1
+print( var > 0 )
+print( not (var <= 0))
+
+#Ejemplo 2
+print(var != 0)
+print(not (var == 0))
