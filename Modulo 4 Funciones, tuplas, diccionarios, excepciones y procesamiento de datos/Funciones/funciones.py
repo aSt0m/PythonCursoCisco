20bc1c1e13adbce2c54d39ac52b9bc568d30b2c6
ast0m <gavomaza1@gmail.com>
2025-02-26 15:14:50 -0600

    Functions: examples

--- Modulo 4 Funciones, tuplas, diccionarios, excepciones y procesamiento de datos/Funciones/funciones.py
+++ Modulo 4 Funciones, tuplas, diccionarios, excepciones y procesamiento de datos/Funciones/funciones.py
@@ -11,3 +11,17 @@ message()
 c = int(input())
 
 print("Los valores ingresados son: ", a, b, c)
+
+
+
+def function_one(param_one, param_two):
+    print("Me gusta la ", param_one, "y el ", param_two, " tambien")
+    
+    
+function_one("Fresa","Platano")
+
+
+def introduction(name, last_name):
+    print("Mi nombre es: ", name, last_name)
+
+introduction("Tom", last_name="Trejo")
