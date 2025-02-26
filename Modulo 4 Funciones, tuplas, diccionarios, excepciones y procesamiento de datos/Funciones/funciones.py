55552df065065242b3693800516598d65d8d1a10
ast0m <gavomaza1@gmail.com>
2025-02-26 14:31:41 -0600

    Functions: my first commit

---
+++ Modulo 4 Funciones, tuplas, diccionarios, excepciones y procesamiento de datos/Funciones/funciones.py
@@ -0,0 +1,13 @@
+""" Funciones """
+
+def message():
+    print("Ingresa un valor")
+    
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+
+print("Los valores ingresados son: ", a, b, c)
