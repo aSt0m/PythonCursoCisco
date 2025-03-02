1b17dd0d5cc3d30dad8943fed6ad938ca8ffdba0
ast0m <gavomaza1@gmail.com>
2025-03-01 23:30:00 -0600

    Functions: Day of year, How many days, prime number

--- Modulo 4 Funciones, tuplas, diccionarios, excepciones y procesamiento de datos/Funciones/funciones.py
+++ Modulo 4 Funciones, tuplas, diccionarios, excepciones y procesamiento de datos/Funciones/funciones.py
@@ -1,27 +1,185 @@
 """ Funciones """
 
-def message():
-    print("Ingresa un valor")
+# def message():
+#     print("Ingresa un valor")
     
-message()
-a = int(input())
-message()
-b = int(input())
-message()
-c = int(input())
+# message()
+# a = int(input())
+# message()
+# b = int(input())
+# message()
+# c = int(input())
 
-print("Los valores ingresados son: ", a, b, c)
+# print("Los valores ingresados son: ", a, b, c)
 
 
 
-def function_one(param_one, param_two):
-    print("Me gusta la ", param_one, "y el ", param_two, " tambien")
+# def function_one(param_one, param_two):
+#     print("Me gusta la ", param_one, "y el ", param_two, " tambien")
     
     
-function_one("Fresa","Platano")
+# function_one("Fresa","Platano")
 
 
-def introduction(name, last_name):
-    print("Mi nombre es: ", name, last_name)
+# def introduction(name, last_name):
+#     print("Mi nombre es: ", name, last_name)
 
-introduction("Tom", last_name="Trejo")
+# introduction("Tom", last_name="Trejo")
+
+
+
+""" Return """
+print('-----return without an expression-----')
+
+# def happy_new_year(wishes = True):
+#     print("Three...")
+#     print("Two")
+#     print("One")
+#     if not wishes:
+#         return
+    
+#     print("Happy New Year!")
+    
+# happy_new_year()
+# print("\n\n")
+
+
+print("-------return with an expression--------")
+
+# def boring_function():
+#     return 123
+
+# x = boring_function()
+
+# print("The boring _function has returned its result. It's:", x)
+
+# print("\n\n")
+
+
+
+print("--------list and functions----------")
+
+
+# def list_sum(lst):
+#     s = 0
+
+#     for elem in lst:
+#         s += elem
+
+#     return s
+
+# print(list_sum([5, 4, 3]))
+
+
+print("-------- LAB Año bisiesto ---------")
+
+# def is_year_leap(year):
+#     if year % 4 != 0:
+#         return False
+#     elif year % 100 != 0:
+#         return True
+#     elif year % 400 != 0:
+#         return False
+#     else:
+#         return True
+
+# test_data = [1536, 2000, 2016, 1987]
+# test_results = [False, True, True, False]
+# for i in range(len(test_data)):
+#     yr = test_data[i]
+#     print(yr,"-> ",end="")
+#     result = is_year_leap(yr)
+#     if result == test_results[i]:
+#         print("OK")
+#     else:
+#         print("Failed")
+
+
+
+print("-------- LAB How many days -------\n")
+
+
+# def is_year_leap(year):           # funcion para el año bisiesto
+#     if year % 4 != 0:
+#         return False
+#     elif year % 100 != 0:
+#         return True
+#     elif year % 400 != 0:
+#         return False
+#     else:
+#         return True
+
+# def days_in_month(year,month):  # funcion para los dias del mes
+#     if year < 1582 or month < 1 or month > 12:
+#         return None
+#     days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     res  = days[month - 1]
+#     if month == 2 and is_year_leap(year):
+#         res = 29
+#     return res
+
+# test_years = [1900, 2000, 2016, 1987]
+# test_months = [ 2, 2, 1, 11]
+# test_results = [28, 29, 31, 30]
+# for i in range(len(test_years)):
+#     yr = test_years[i]
+#     mo = test_months[i]
+#     print(yr,mo,"-> ",end="")
+#     result = days_in_month(yr, mo)
+#     if result == test_results[i]:
+#         print("OK")
+#     else:
+#         print("Failed")
+        
+        
+        
+print("------ Day of the year -------")
+
+def is_year_leap(year):
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+
+def days_in_month(year, month):
+    if year < 1582 or month < 1 or month > 12:
+        return None
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    res  = days[month - 1]
+    if month == 2 and is_year_leap(year):
+        res = 29
+    return res
+
+def day_of_year(year, month, day):
+    days = 0
+    for m in range(1, month):
+        md = days_in_month(year, m)
+        if md == None:
+            return None
+        days += md
+    md = days_in_month(year, month)
+    if day >= 1 and day <= md:
+        return days + day
+    else:
+        return None
+
+print(day_of_year(2000, 12, 30))
+
+
+
+print("------- Prime numbers ------")
+
+def is_prime(num):
+    for i in range(2, int(1 + num ** 0.5)):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
+print()
