7ad2b4261d88838576db1f8cf1df5e9ce1a1a75a
ast0m <gavomaza1@gmail.com>
2025-03-03 19:00:38 -0600

    Functions: Triangulos, IMC

--- Modulo 4 Funciones, tuplas, diccionarios, excepciones y procesamiento de datos/Funciones/funciones.py
+++ Modulo 4 Funciones, tuplas, diccionarios, excepciones y procesamiento de datos/Funciones/funciones.py
@@ -135,51 +135,121 @@ print("-------- LAB How many days -------\n")
         
 print("------ Day of the year -------")
 
-def is_year_leap(year):
-    if year % 4 != 0:
-        return False
-    elif year % 100 != 0:
-        return True
-    elif year % 400 != 0:
-        return False
-    else:
-        return True
+# def is_year_leap(year):
+#     if year % 4 != 0:
+#         return False
+#     elif year % 100 != 0:
+#         return True
+#     elif year % 400 != 0:
+#         return False
+#     else:
+#         return True
 
-def days_in_month(year, month):
-    if year < 1582 or month < 1 or month > 12:
-        return None
-    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    res  = days[month - 1]
-    if month == 2 and is_year_leap(year):
-        res = 29
-    return res
+# def days_in_month(year, month):
+#     if year < 1582 or month < 1 or month > 12:
+#         return None
+#     days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     res  = days[month - 1]
+#     if month == 2 and is_year_leap(year):
+#         res = 29
+#     return res
 
-def day_of_year(year, month, day):
-    days = 0
-    for m in range(1, month):
-        md = days_in_month(year, m)
-        if md == None:
-            return None
-        days += md
-    md = days_in_month(year, month)
-    if day >= 1 and day <= md:
-        return days + day
-    else:
-        return None
+# def day_of_year(year, month, day):
+#     days = 0
+#     for m in range(1, month):
+#         md = days_in_month(year, m)
+#         if md == None:
+#             return None
+#         days += md
+#     md = days_in_month(year, month)
+#     if day >= 1 and day <= md:
+#         return days + day
+#     else:
+#         return None
 
-print(day_of_year(2000, 12, 30))
+# print(day_of_year(2000, 12, 30))
 
 
 
 print("------- Prime numbers ------")
 
-def is_prime(num):
-    for i in range(2, int(1 + num ** 0.5)):
-        if num % i == 0:
-            return False
-    return True
+# def is_prime(num):
+#     for i in range(2, int(1 + num ** 0.5)):
+#         if num % i == 0:
+#             return False
+#     return True
 
-for i in range(1, 20):
-    if is_prime(i + 1):
-        print(i + 1, end=" ")
-print()
+# for i in range(1, 20):
+#     if is_prime(i + 1):
+#         print(i + 1, end=" ")
+# print()
+
+
+
+print("------ Indice de masa corporal ------")
+
+# funcion que me da el indice de masa corporal
+
+# def imc(weight, height):
+#     imc = weight / height ** 2
+#     print("El indice de masa corporal es: ", imc )
+# print(imc(3, 2))
+
+# funcion que realiza la conversion de libras a kilogramo
+# sabemos que que 1 lb =  0.45359237kg
+
+# def ft_and_inch_to_m(ft, inch = 0.0):
+#     return ft * 0.3048 + inch * 0.0254
+
+
+# def lb_to_kg(lb):
+#     return lb * 0.4535923
+
+
+# def bmi(weight, height):
+#     if height < 1.0 or height > 2.5 or weight < 20 or weight > 200:
+#         return None
+
+#     return weight / height ** 2
+
+
+# print(bmi(weight = lb_to_kg(176), height = ft_and_inch_to_m(5, 7)))
+
+print("------ Ejemplos de funciones: Triangulos ------")
+
+
+
+# def is_triangle(a, b, c):
+#     return a + b > c and b + c > a and c + a > b
+
+# a = int(input("Dame el valor de a: "))
+# b = int(input("Dame el valor de b: "))
+# c = int(input("Dame el valor de c:  "))
+
+# print(is_triangle(a,b,c))
+
+# if is_triangle(a, b, c):
+#     print("Si, si puede ser un triangulo")
+# else:
+#     print("No, no puede ser un triangulo")
+    
+    
+    
+ 
+print("------ Triangulos y el Teorema de Pitagoras ------ ") 
+     
+# def is_a_triangle(a, b, c):
+#     return a + b > c and b + c > a and c + a > b
+
+
+# def is_a_right_triangle(a, b, c):
+#     if not is_a_triangle(a, b, c):
+#         return False
+#     if c > a and c > b:
+#         return c ** 2 == a ** 2 + b ** 2
+#     # if a > b and a > c:
+#     if a > b and a > c:
+#         return a ** 2 == b ** 2 + c ** 2
+# print(is_a_right_triangle(5, 3, 4))
+# print(is_a_right_triangle(1, 3, 4))
+
